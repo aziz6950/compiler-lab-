4ac324d2be62257ce8b8380678ca2b376a1ac2b1
Nurul Aziz <sohelcse233151@gmail.com>
2025-11-17 22:19:06 +0600

    Add regex for email validation

--- e3.cpp
+++ e3.cpp
@@ -7,7 +7,7 @@ int main() {
     cout << "Enter email: ";
     cin >> email;
 
-    // Simple email regex
+   
     regex email_regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{1,}$");
 
     if (regex_match(email, email_regex)) {
